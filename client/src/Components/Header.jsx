e8491503fac844d6dfd61c7bdb11c39840ec78c7
Andrea Alessandro Gaudino <andreasmacbookpro@MBP-di-Andrea.Home>
2023-10-22 17:20:24 +0200

    Ticket starting

---
+++ client/src/Components/Header.jsx
@@ -0,0 +1,13 @@
+function Header() {
+  return (
+    <nav class="navbar bg-dark border-bottom border-body">
+      <div class="container-md">
+        <a class="navbar-brand" href="#">
+          Navbar
+        </a>
+      </div>
+    </nav>
+  );
+}
+
+export default Header;
---
+++ client/src/Pages/GetTicketPage.jsx
@@ -0,0 +1,7 @@
+function GetTicketPage() {
+    return (
+        <h1>Get the ticket Page</h1>
+    );
+}
+
+export default GetTicketPage;
