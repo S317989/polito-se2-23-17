fefc01e6b6c42ec9cfa9fb68ab91bfddc4a95743
Federico Sanfrancesco <s308344@studenti.polito.it>
2023-10-26 13:09:56 +0200

    addes service list and counters

--- client/src/Pages/Services.jsx
+++ client/src/Pages/Services.jsx
@@ -1,4 +1,4 @@
-import {Container, Col, Row, ListGroupItem} from 'react-bootstrap';
+import {Container, Col, Row} from 'react-bootstrap';
 import Pagination from 'react-bootstrap/Pagination';
 import ListGroup from 'react-bootstrap/ListGroup';
 import ServiceAPI from '../APIs/ServiceAPI';
@@ -8,21 +8,24 @@ function Services() {
 
    const counter=[1,2,3,4]; //counter is a fixed number
    const [active, setActive]=useState(1);
+   const [clist, setClist]=useState();
    
    const [slist, setSlist]=useState();
 
-  
 
 
+    
 
     const handlelistService = () => {
       ServiceAPI.getServiceList().then(async (response) => {
         const data = await response.json();
   
         if (response.status === 200) {
-          const sList=data.map((i)=>{id: i.id, name: i.name, ast: i.ast});
+            const sList = data.map((i) => ({ id: i.id, name: i.name, ast: i.ast }));
+
           setSlist(sList);
         } else {
+            
           console.log("Error");
         }
       });
@@ -34,12 +37,17 @@ function Services() {
     }
     ,[] )
     
-    handleCounterServices = (cId)=>{
+    const handleCounterServices = (cId)=>{
 
         ServiceAPI.getServiceListByCounter(cId).then(async (response)=>{
             const data = await response.json;
             if(response.status ===200) {
-                const clist=data.map((e)=>{id: i.id, name: i.name, ast: i.ast});
+                const counterlist = data.map((e) => ({ id: e.id, name: e.name, ast: e.ast }));
+
+                setClist(counterlist);
+            }
+            else {
+                console.log("error")
             }
         })
     }
@@ -51,28 +59,40 @@ function Services() {
             <Col>
                  <ListGroup as="ul">
                 {
-                    counterslist ?
-                    counterslist.map((e)=><ListGroup.Item as='li' key={e.id}  >{e.name} with Average Service Time: {e.ast}</ListGroup.Item>)
+                    slist ?
+                    slist.map((e)=><ListGroup.Item as='li' key={e.id}> {e.name} with Average Service Time: {e.ast}</ListGroup.Item>)
                     :
                     <h3>Services not available</h3>
+                    
                 }
             
             </ListGroup>
 
             </Col>
             <Col>
+            <Row>
             {
-                counter.map((e)=><Pagination.Item key={e} active={e === active} onClick={()=>setActive(e)} >
+                counter.map((e)=><Pagination.Item key={e} active={e === active} onClick={()=>{
+                    setActive(e);
+                    handleCounterServices(e);
+                    
+                    }} >
                 Counter {e}
                 </Pagination.Item>)
+                
             }
-            {
-            active==1?
-            //function to call handleCounter and retrive services for counters
+            </Row>
+            <Row>
+            <ListGroup as="ul">
+            {   
+                 clist ? //dovrebbe aggiornarsi in automatico
+                    clist.map((e)=><ListGroup.Item as='li' key={e.id} >{e.name} with Average Service Time: {e.ast}</ListGroup.Item>)
                 :
-
-            }
-        
+                <h3>Couldn't retrieve services</h3>
+            
+            }   
+            </ListGroup>
+            </Row>
             </Col>
                 
            
