a3253bac291ebaa42bc91630fc15a363a9cee429
Gizem Irmak <s314140.studenti.polito.it>
2023-10-26 22:55:23 +0200

    final version stats.

--- server/Controllers/StatsController.js
+++ server/Controllers/StatsController.js
@@ -26,24 +26,4 @@ module.exports = {
 };
 
 
-// module.exports = {
-//     getServiceTypeStats: async function(req, res) {
-//         try {
-//             const { timePeriod } = req.params; // 'day', 'week', 'month'
-//             const stats = await statsDAO.getServiceTypeStats(timePeriod);
-//             return res.status(200).json({ stats });
-//         } catch (error) {
-//             return res.status(500).json({ message: error.message });
-//         }
-//     },
 
-//     getCounterServiceStats: async function(req, res) {
-//         try {
-//             const { timePeriod } = req.params; // 'day', 'week', 'month'
-//             const stats = await statsDAO.getCounterServiceStats(timePeriod);
-//             return res.status(200).json({ stats });
-//         } catch (error) {
-//             return res.status(500).json({ message: error.message });
-//         }
-//     },
-// };
